6e1cea6a4ec45b9f568022fcce5f013c20a9ecdd
jacoblbagent <jacoblbagent@users.noreply.github.com>
2026-08-13 10:08:49 -0400

    remove footer__brand

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import './Footer.scss'
 export function Footer() {
   return (
     <footer className="footer">
-      <span className="footer__brand">Blockcraft</span>
       <span className="footer__note">
         A fan-made showcase of Minecraft builds. Not affiliated with Mojang or Microsoft.
       </span>
